abf5d9df0bba8b3ae1b894807356cbc4882d52a0
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-09 20:39:49 -0700

    LinkedList | Added append function

--- src/data_structures/linked_list/linkedList.js
+++ src/data_structures/linked_list/linkedList.js
@@ -1,7 +1,23 @@
+const linkedListNode = require("./linkedListNode");
+
 function linkedList() {
 	return {
 		head: null,
 		tail: null,
+		append(value) {
+			const node = linkedListNode(value);
+
+			if (!this.head) {
+				this.head = node;
+				this.tail = node;
+				return this;
+			}
+
+			this.tail.next = node;
+			this.tail = node;
+
+			return this;
+		},
 		toArray() {
 			const nodes = [];
 			let currentNode = this.head;
@@ -14,7 +30,9 @@ function linkedList() {
 			return nodes;
 		},
 		toString(cb) {
-			return this.toArray().map((node) => node.toString(cb)).toString();
+			return this.toArray()
+				.map((node) => node.toString(cb))
+				.toString();
 		},
 	};
 }
--- src/data_structures/linked_list/linkedList.test.js
+++ src/data_structures/linked_list/linkedList.test.js
@@ -1,8 +1,21 @@
-const linkedList = require("./linkedList")
+const linkedList = require("./linkedList");
 
 describe("LinkedList", () => {
 	it("should create empty linked list", () => {
 		const ll = linkedList();
 		expect(ll.toString()).toBe("");
+	});
+
+	it("should be able to append node to linked list", () => {
+		const ll = linkedList();
+
+		expect(ll.head).toBeNull();
+		expect(ll.tail).toBeNull();
+
+		ll.append(1);
+		ll.append(2);
+
+		expect(ll.toString()).toBe("1,2");
+		expect(ll.tail.next).toBeNull();
 	});
 });
