fca0c6e5e039fb1b7fb02030bf80803aa28114bb
Philip Nguyen <philipnguyen953@gmail.com>
2023-05-14 21:25:57 -0700

    MergeSort | Added additional test

--- src/algorithms/merge_sort/mergeSort.test.js
+++ src/algorithms/merge_sort/mergeSort.test.js
@@ -10,4 +10,9 @@ describe("MergeSort", () => {
     const array = [-1, 3, 2, -5, 4];
     expect(mergeSort(array).toString()).toBe("-5,-1,2,3,4");
   });
+
+  it("should sort array with negative numbers, zero, and duplicates", () => {
+    const array = [-1, 0, 3, 2, -5, 4, 0, 0];
+    expect(mergeSort(array).toString()).toBe("-5,-1,0,0,0,2,3,4");
+  });
 });
