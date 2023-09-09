826038f9086c7667e4fb855e04dd19d9eb435d66
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-08 18:42:09 -0700

    Added stack and tests

---
+++ src/data_structures/stack/stack.js
@@ -0,0 +1,47 @@
+const stackNode = require("./stackNode");
+
+function stack() {
+  return {
+    length: 0,
+    head: undefined,
+    push(item) {
+      const node = stackNode(item);
+      this.length++;
+
+      if (!this.head) {
+        this.head = node;
+        return;
+      }
+
+      this.head.next = node;
+      node.prev = this.head;
+      this.head = node;
+    },
+    pop() {
+      if (this.length === 0) {
+        return undefined;
+      }
+
+      if (this.length === 1 && this.head.prev) {
+        const val = this.head.value;
+        this.head = undefined;
+        this.length--;
+        return val;
+      }
+
+      if (this.head && this.head.value) {
+        this.length--;
+        const val = this.head.value;
+        this.head = this.head.prev;
+        this.head.next = undefined;
+        return val;
+      }
+    },
+    peek() {
+      if (this.head) return this.head.value;
+      return undefined;
+    },
+  };
+}
+
+module.exports = stack;
---
+++ src/data_structures/stack/stack.test.js
@@ -0,0 +1,35 @@
+const { default: expect } = require("expect");
+const stack = require("./stack");
+
+describe("Stack", () => {
+    it("should be able to create a stack", () => {
+        const s = stack();
+        expect(s.peek()).toBeUndefined();
+    })
+
+    it("should be able to push", () => {
+        const s = stack();
+        
+        s.push(51);
+        s.push(52);
+        s.push(25);
+        s.push(215);
+
+        expect(s.peek()).toBe(215);
+        expect(s.length).toBe(4);
+    })
+
+    it("should be able to pop", () => {
+        const s = stack();
+        
+        s.push(51);
+        s.push(52);
+        s.push(25);
+        s.push(215);
+        s.pop();
+        s.pop();
+
+        expect(s.peek()).toBe(52);
+        expect(s.length).toBe(2);
+    })
+})
