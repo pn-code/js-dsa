95a4ff6854ef72d34ac5ed9bdcbf96faf8e6fee6
Philip Nguyen <philipnguyen953@gmail.com>
2023-09-17 20:44:42 -0700

    Set up Tree

---
+++ src/data_structures/bst/Tree.js
@@ -0,0 +1,12 @@
+const TreeNode = require("./TreeNode");
+
+function BST() {
+    return {
+        terminus: null,
+        find(target) {},
+        insert(value) {},
+        delete(value) {},
+    };
+}
+
+module.exports = BST;
